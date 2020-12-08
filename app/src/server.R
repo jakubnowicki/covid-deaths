110915a4446e79cd70e7607f51ccd0e9e5bbdcb2
Jakub Nowicki <kuba@appsilon.com>
2020-12-08 22:16:45 +0100

    Second chart

--- app/src/server.R
+++ app/src/server.R
@@ -1,7 +1,7 @@
 function(input, output, session) {
   session$userData$data <- data
 
-  output$chart <- echarts4r::renderEcharts4r({
+  output$general_chart <- echarts4r::renderEcharts4r({
     selected_area <- input$area
     if (is.null(selected_area)) {
       selected_area <- "Polska"
@@ -15,7 +15,7 @@ function(input, output, session) {
     data <- session$userData$data %>%
       filter(area %in% selected_area, age %in% selected_age)
 
-    if (!is.null(input$grouping)) {
+    if (input$grouping != "none") {
       data <- data %>%
       group_by(
         across(input$grouping)
@@ -27,7 +27,7 @@ function(input, output, session) {
       summarise(deaths = sum(deaths)) %>%
       ungroup()
 
-    if (!is.null(input$grouping)) {
+    if (input$grouping != "none") {
       data <- data %>%
       group_by(
         across(input$grouping)
@@ -42,6 +42,45 @@ function(input, output, session) {
         deaths
       ) %>%
       e_tooltip() %>%
-      e_datazoom(type = "slider")
+      e_datazoom(type = "slider") %>%
+      e_title("Deaths in time", left = "10%")
+  })
+
+  output$year_comparison_chart <- echarts4r::renderEcharts4r({
+    selected_area <- input$area
+    if (is.null(selected_area)) {
+      selected_area <- "Polska"
+    }
+
+    selected_age <- input$age
+    if (is.null(selected_age)) {
+      selected_age <- "Ogółem"
+    }
+
+    selected_years <- input$years
+    if (is.null(selected_years)) {
+      selected_years <- years
+    }
+
+    data <- session$userData$data %>%
+      filter(area %in% selected_area, age %in% selected_age, year %in% selected_years)
+
+
+    data <- data %>%
+      group_by(year, week) %>%
+      summarise(deaths = sum(deaths)) %>%
+      ungroup()
+
+    data %>%
+      group_by(year, .add = TRUE) %>%
+      e_chart(
+        x = week
+      ) %>%
+      e_line(
+        deaths
+      ) %>%
+      e_tooltip() %>%
+      e_datazoom(type = "slider") %>%
+      e_title("Comparison by year", left = "10%")
   })
 }
--- app/src/ui.R
+++ app/src/ui.R
@@ -2,11 +2,13 @@ semanticPage(
   shiny.semantic::grid(
     grid_template = grid_template(
       default = list(
-        areas = rbind("chart", "controls"),
-        rows_height = c("5fr", "2fr")
+        areas = cbind(c("controls", "controls"), c("general_chart", "year_comparison_chart")),
+        cols_width = c("1fr", "5fr"),
+        rows_height = c("1fr", "1fr")
       )
     ),
-    chart = echarts4r::echarts4rOutput("chart", height = "100%"),
+    general_chart = echarts4r::echarts4rOutput("general_chart", height = "100%"),
+    year_comparison_chart = echarts4r::echarts4rOutput("year_comparison_chart", height = "100%"),
     controls = div(
       shiny.semantic::selectInput(
         inputId = "area",
@@ -25,8 +27,15 @@ semanticPage(
       shiny.semantic::selectInput(
         inputId = "grouping",
         label = "Group",
-        choices = c("area", "age", "year"),
-        multiple = TRUE
+        choices = c("none", "age", "area"),
+        multiple = FALSE
+      ),
+      shiny.semantic::selectInput(
+        inputId = "years",
+        label = "Years to compare",
+        choices = years,
+        multiple = TRUE,
+        selected = tail(sort(years), 2)
       )
     )
   )
