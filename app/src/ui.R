2ff04c8104d281570848f87322a6c7868c18c9d6
Jakub Nowicki <kuba@appsilon.com>
2021-04-19 23:24:00 +0200

    Remove shiny.infor from code.

--- app/src/ui.R
+++ app/src/ui.R
@@ -118,13 +118,13 @@ semanticPage(
       )
     )
   ),
-  shiny.info::display(
+  div(
+    class = "info-box",
     span(
       "Created by Jakub Nowicki",
       tags$a(href = "https://twitter.com/q_nowicki", icon("twitter")),
       tags$a(href = "https://www.linkedin.com/in/jakub-nowicki/", icon("linkedin")),
       tags$a(href = "https://github.com/jakubnowicki", icon("github"))
-    ),
-    position = "bottom right"
+    )
   )
 )
